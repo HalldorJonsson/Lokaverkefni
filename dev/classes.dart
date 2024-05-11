800c1b29d9de197d2e554a555637afad3e6891c4
HalldorJonsson <160058960+HalldorJonsson@users.noreply.github.com>
2024-05-11 12:51:27 +0000

    classes

--- dev/classes.dart
+++ dev/classes.dart
@@ -2,31 +2,54 @@ import 'dart:io';
 import 'main.dart';
 
 
-class User { //breyta user?
+class User {
+  //breyta user?
   final String playerName;
   final String companionName;
-  final String description;
-  final Map<String, User> exits;
-  final List<String> items;
 
   User({
     required this.playerName,
     required this.companionName,
-    required this.description,
-    required this.exits,
-    required this.items,
+    //required this.description,
+    //required this.exits,
+    //required this.items,
   });
-
 }
+  class Room {
+  final String name;
+  final String description;
+  final Map<String, Room> exits;
+  final List<String> items;
+
+  Room({
+  required this.name,
+  required this.description,
+  required this.exits,
+  required this.items,
+  });
+  }
+
+
 class Player { //????
   List<String> inventory = [];
 }
 
 class Game {
-  late User currentRoom;
-  late Map<String, User> rooms;
+  late Room currentRoom;
+  late Map<String, Room> rooms;
   late Player player;
+
+  Game() {
+    // Define rooms
+    Room room1 = Room(
+      name: 'Room 1',
+      description: 'You are in room 1. There is a door to the north.',
+      exits: {},
+      items: ['key'],
+    );
 
 }
 
+
+}
 //teikna upp room plan. Hvert fara exits og annað. (nýtt file?)
